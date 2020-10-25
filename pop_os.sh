4f1a1008a88db6488e1e8918002acdbf80277826
Nicholas Rodrigues <rodriguesnich@outlook.com>
2020-10-25 14:44:09 -0300

    Update pop_os.sh

--- pop_os.sh
+++ pop_os.sh
@@ -1,56 +1,92 @@
 #!/bin/bash
 
-echo "test"
-
+## Removendo Travas APT ##
 sudo rm /var/lib/dpkg/lock-frontend ; sudo rm /var/cache/apt/archives/lock ;
 
-## sudo apt update && ##
+## Atualizando Sistema ##
+sudo apt update -y
 
-sudo apt install python3 python3-pip
+## Instalando Snap ##
+echo "Instalando Snap"
+sudo apt install snapd -y
 
-echo "Instalando Snap com Apt..."
-sudo apt install snapd
 
-echo "Instalando Brave com Apt..."
-sudo apt install apt-transport-https curl
+## DEV ##
 
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+## Python3 e pip ##
+echo "Instalando Python3 e python3-pip"
+sudo apt install python3 python3-pip -y
 
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-
-sudo apt install brave-browser
-
-## FlatPack ##
-echo "Instalando VS-Code com Flatpack..."
+## VS Code ##
+echo "Instalando VS-Code"
 flatpak install flathub com.visualstudio.code
 
-echo "Instalando Postman com Flatpack..."
+## Postman ##
+echo "Instalando Postman"
 flatpak install flathub com.getpostman.Postman
 
-echo "Instalando Discord com Flatpack..."
-flatpak install flathub com.discordapp.Discord
-
-echo "Instalando Steam com Flatpack..."
-flatpak install flathub com.valvesoftware.Steam
-
-echo "Instalando Skype com Flatpack..."
-flatpak install flathub com.skype.Client
-
-echo "Instalando InkScape com Flatpack..."
-flatpak install flathub org.inkscape.Inkscape
-
-echo "Instalando Spotify com Flatpack..."
-flatpak install flathub com.spotify.Client
-
-
-echo "Instalando Beekeeper-Studio com Snap..."
+## Beekeeper ##
+echo "Instalando Beekeeper-Studio"
 sudo snap install beekeeper-studio
 
-echo "Instalando Photo-Gimp com Snap..."
-sudo snap install photogimp
+## NodeJS ##
+echo "Instalando o NodeJS"
+sudo apt install nodejs -y
+
+## npm ##
+echo "Instalando npm"
+sudo apt install npm -y
 
 echo "Atualizando Node"
 sudo npm install -g n
+sudo n stable
+
+
+
+
+## SYS ##
+
+## Discord ##
+echo "Instalando Discord"
+flatpak install flathub com.discordapp.Discord
+
+## Steam ##
+echo "Instalando Steam"
+flatpak install flathub com.valvesoftware.Steam
+
+## Skype ##
+echo "Instalando Skype"
+flatpak install flathub com.skype.Client
+
+## Spotify ##
+echo "Instalando Spotify"
+flatpak install flathub com.spotify.Client
+
+## Brave ##
+echo "Instalando Brave"
+sudo snap install brave
+
+
+## DESING ##
+
+## Inkscape ##
+echo "Instalando InkScape"
+flatpak install flathub org.inkscape.Inkscape
+
+## Photo-Gimp ##
+echo "Instalando Photo-Gimp"
+sudo snap install photogimp
+
+
+
+
+
+
+
+
+
+
+
 
 ## Criacao de Pastas ##
 
@@ -62,7 +98,9 @@ mkdir/home/$USER/programing
 
 mkdir/home/$USER/programing/study
 
-mkdir/home/$USER/programing/projects
+mkdir/home/$USER/programing/code
+
+mkdir/home/$USER/programing/tecnojr
 
 ## Pastas de Imagens ##
 mkdir/home/$USER/Pictures/projects
@@ -73,6 +111,19 @@ mkdir/home/$USER/Documents/projects
 ## Baixar Programas Externos ##
 
 cd /home/$USER/Downloads/
+
+## Chrome ##
+wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+## 4k video downloader ##
+wget -c https://dl.4kdownload.com/app/4kvideodownloader_4.13.3-1_amd64.deb?source=website
+
+## 4k music downloader ##
+wget -c https://dl.4kdownload.com/app/4kyoutubetomp3_3.13.2-1_amd64.deb?source=website
+
+
+## Instala todos os .deb ##
+sudo dpkg -i *.deb
 
 
 ## Finalizando Script ##

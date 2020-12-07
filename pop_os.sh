6938c702dea54ea291ba351b1f7045912130e482
Nicholas Rodrigues <rodriguesnich@outlook.com>
2020-12-07 07:47:24 -0300

    test

--- pop_os.sh
+++ pop_os.sh
@@ -41,7 +41,38 @@ echo "Atualizando Node"
 sudo npm install -g n
 sudo n stable
 
+## MySQL ##
+echo "Instalando MySQL"
+sudo apt install mysql-server -y
 
+## PHP ##
+echo "Instalando PHP e Apache"
+sudo apt-get install apache2 -y
+sudo systemctl start apache2.service
+
+apt-cache pkgnames | grep php7.2
+
+sudo apt-get install php -y
+sudo apt-get install php-{bcmath,bz2,intl,gd,mbstring,mysql,zip,fpm} -y
+
+systemctl restart apache2.service
+
+php -v
+
+## Composer ##
+echo "Instalando Composer"
+sudo apt install php-cli unzip
+cd ~
+
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+
+HASH=`curl -sS https://composer.github.io/installer.sig`
+
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
+composer
 
 
 ## SYS ##
@@ -65,6 +96,10 @@ flatpak install flathub com.spotify.Client
 ## Brave ##
 echo "Instalando Brave"
 sudo snap install brave
+
+## Torrent ##
+echo "Instalando QBit Torrent"
+flatpak install flathub org.qbittorrent.qBittorrent
 
 
 ## DESING ##
@@ -121,6 +156,14 @@ wget -c https://dl.4kdownload.com/app/4kvideodownloader_4.13.3-1_amd64.deb?sourc
 ## 4k music downloader ##
 wget -c https://dl.4kdownload.com/app/4kyoutubetomp3_3.13.2-1_amd64.deb?source=website
 
+## FireFox Dev ## 
+wget -c "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=pt-BR" -O firefox-developer.tar.bz2
+
+sudo tar -jxvf  firefox-developer.tar.bz2 -C /opt/
+
+sudo mv /opt/firefox*/ /opt/firefox-developer
+
+echo -e '[Desktop Entry]\n Version=59.0.3\n Encoding=UTF-8\n Name=Mozilla Firefox\n Comment=Navegador Web\n Exec=/opt/firefox-developer/firefox\n Icon=/opt/firefox-developer/browser/chrome/icons/default/default128.png\n Type=Application\n Categories=Network' | sudo tee /usr/share/applications/firefox-developer.desktop
 
 ## Instala todos os .deb ##
 sudo dpkg -i *.deb
